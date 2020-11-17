e7f1d825d0bcfefa798bea9b33394852f6e2ccd5
Sy Rashid <sy.rashid.11@gmail.com>
2020-11-17 09:46:38 +0100

    FEAT: Added index action for pokemon

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -1,2 +1,9 @@
 class Pokemon < ApplicationRecord
+  TYPES = %w(Normal Fighting Flying Poison Ground Rock Bug Ghost Fire Water Grass Electric Steel Psychic Ice Dragon Dark)
+  validates :name, presence: true
+  validates :species, inclusion: { in: TYPES }
+
+  def found_days_ago
+    (Date.today - found_on).to_i
+  end
 end
